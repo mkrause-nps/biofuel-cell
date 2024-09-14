b9e63e0a4a9da147220151db8ed6843bde944859
Michael Krause <michael.krause@nps.edu>
2024-09-14 16:25:40 -0700

    Add spreadsheet class

--- src/spreadsheet.py
+++ src/spreadsheet.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
+
+class Spreadsheet:
+    pass
+
 if __name__ == '__main__':
     pass
--- test/test_spreadsheet.py
+++ test/test_spreadsheet.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python3
 import unittest
+from src.spreadsheet import Spreadsheet
+
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
+
 
 if __name__ == '__main__':
     unittest.main()
