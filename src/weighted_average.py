071171167bfb55b96120d659050510d593ade088
Michael Krause <michael.krause@nps.edu>
2024-09-17 10:52:12 -0700

    Add weighted average class with test

---
+++ src/weighted_average.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+from typing import List, Tuple
+import pandas as pd
+
+
+class WeightedAverage(object):
+
+    def __init__(self, df: pd.DataFrame, num_obs: int) -> None:
+        if len(df.columns) != 4:
+            raise ValueError("Expected 4 columns, got {}".format(df.columns))
+        self.df: pd.DataFrame = df
+        self.column_names: list = df.columns.tolist()
+        self.values: pd.Series = df[df.columns[2]]
+        self.std_devs: pd.Series = df[df.columns[3]]
+        self.num_obs: int = num_obs
+
+    def get_weighted_average(self) -> pd.DataFrame:
+        experiment_ids: str = self.column_names[0]
+        values: str = self.column_names[2]
+        std_dev: str = self.column_names[3]
+        grouped_stats = self.df.groupby(experiment_ids).apply(
+            lambda x: pd.Series(self.weighted_stats(x[values], x[std_dev], self.num_obs))).reset_index()
+        grouped_stats.columns = [experiment_ids, 'weighted_average_value', 'weighted_std_dev']
+        return grouped_stats
+
+    @staticmethod
+    def weighted_stats(values, std_devs, n):
+        weights = [n / (std ** 2) for std in std_devs]
+        weighted_avg = sum(value * weight for value, weight in zip(values, weights)) / sum(weights)
+
+        # Calculate the weighted variance.
+        weighted_variance = sum(
+            weight * (value - weighted_avg) ** 2 for value, weight in zip(values, weights)) / sum(
+            weights)
+
+        # Calculate the weighted standard deviation.
+        weighted_std_dev = weighted_variance ** 0.5
+
+        return weighted_avg, weighted_std_dev
+
+    # def _get_weights(self):
+    #     return [self.num_obs / (std ** 2) for std in self.std_devs]
+    #
+    # def _get_weighted_average(self):
+    #     weights = self._get_weights()
+    #     return sum(value * weight for value, weight in zip(self.values, weights)) / sum(weights)
+    #
+    # def _get_weighted_variance(self):
+    #     """Calculate the weighted variance."""
+    #     weighted_avg = self._get_weights()
+    #     weights = self._get_weights()
+    #     return sum(weight * (value - weighted_avg) ** 2 for value, weight in zip(self.values, weights)) / sum(weights)
+    #
+    # def _get_weighted_standard_deviation(self):
+    #     weighted_variance = self._get_weighted_variance()
+    #     return weighted_variance ** 0.5
